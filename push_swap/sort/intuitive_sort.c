26a938f45bc770ab4baf437f22bd12e63bed99cf
Matthew Vogee <mvogee@Matthews-MacBook-Pro.local>
2017-02-14 11:31:52 -0800

    progress on sorting algorithm

--- push_swap/sort/intuitive_sort.c
+++ push_swap/sort/intuitive_sort.c
@@ -1,32 +1,117 @@
 // 42 header here
 
 #include "push_swap.h"
-// first attempt at sorting algorithm. my own brain baby version
+// first attempt at sorting algorithm. my own brain baby intuitive sorting method
+
+int		check_sorted(t_stack *stack)
+{
+	t_stack		*cpy;
+
+	cpy = stack;
+	while (cpy->next)
+	{
+		if (cpy->value > cpy->next->value)
+			return (0);
+		cpy = cpy->next;
+	}
+	return (1);
+}
+
+int		find_push_cutoff(t_all *all)
+{
+	int		end;
+	int		mid;
+	int		count;
+	t_stack	*tmp;
+
+	tmp = all->stacka;
+	count = 0;
+	while (tmp->next)
+	{
+		count++;
+		if (count = (all->sizea / 2))
+			mid = tmp->value;
+		tmp = tmp->next;
+	}
+	end = tmp->value;
+	if (all->stacka->value < mid && mid < end)
+		return (mid);
+	else if (mid < all->stacka->value && all->stacka->value < end)
+		return (all->stacka->value);
+	else
+		return (end);
+}
+
+int		check_swapa(t_stack *stacka)
+{
+	if (stacka->next)
+	{
+		if (stacka->value > stacka->next->value)
+			return (SA);
+	}
+	return (0);
+}
+
+int		check_swapb(t_stack *stackb)
+{
+	if (stackb->next)
+	{
+		if (stackb->value < stackb->next->value)
+			return (SB);
+	}
+	return (0);
+}
+
+void	do_actions(t_all **all, int a, int b)
+{
+	if (a == SA && b == SB)
+	{
+		ft_printf("ss\n");
+		do_swap(&all->stacka);
+		do_swap(&all->stackb);
+	}
+	else if (a == SA)
+	{
+		ft_printf("sa\n");
+		do_swap(&all->stacka);
+	}
+	else if (b == SB)
+	{
+		ft_printf("sb\n");
+		do_swap(&all->stackb);
+	}
+	else if (a == RRA)
+	{
+		ft_printf("rra\n");
+		do_reverse_rotate(&all->stacka);
+	}
+}
+
 void	push_swap(t_all *all)
 {
-	int split;
-	int sorted;
-	int	b_action; // these tell what action needs to be taken for a and b
-	int	a_action;
-	// 0 = do nothing
-	// we have enums for the rest of the actions
-	//if a or b needs to be rotated can we wait on that command until we know we cant do it alongside the other stack
-	sorted = check_sorted(stacks); // make this. return 1 if sorted else 0
-	split = find_push_cutoff(all->stacka); // manually sort and get the middle number
-	
-	while (!sorted)
+	int		split;
+	int		sorted;
+	int		b_action; // these tell what action needs to be taken for a and b
+	int		a_action;
+		//action  0 = do nothing
+	sorted = check_sorted(all->stacka); // make this. return 1 if sorted else 0
+	split = find_push_cutoff(all); // takes the median of 3 numbers from the stack
+	while (!sorted) // could add a check_special_cases function in here
 	{
-		a_action = check_top_stacka_for_swap;
-		b_action = check_top_stackb_for_swap;
-		if (actions)
-			do the actions
-		if (sorted = check_sorted(all))
-			break ;
-		if (stacks->stacka->value < split)
+		a_action = check_swapa(all->stacka);
+		b_action = check_swapb(all->stackb);
+		if (a_action || b_aciton)
+			do_actions(&all, a_aciton, b_action); and print action if only b_action is active dont execute yet
+		sorted = check_sorted(all);
+		if (all->stacka->value <= split && !sorted)
 		{
-			push_b
-			b_action = get_action()
+			do_push(&all->stackb, &all->stacka); // these will need to be modded for case where one o
+			//b_action = get_b_action() // make this
 		}
+		if (!sorted)
+			do_actions(&all, RRA, 0);
+		else
+			finish_b(&all); // make this. make sure b is sorted and push all elems to a
 	}
 }
 

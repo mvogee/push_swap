d2892d2edac5ddb491749a75d2e3559158f229d5
Matthew Vogee <mvogee@Matthews-MacBook-Pro.local>
2017-02-21 09:36:37 -0800

    start_pushback and sort_b

--- push_swap/sort/experimental.c
+++ push_swap/sort/experimental.c
@@ -55,6 +55,69 @@ int		get_lastval(t_stack *stack)
 	return (tmp->value);
 }
 
+void	sort_b(t_stack **stack);
+{
+	int			sorted;
+	int			count;
+
+	count = 0;
+	sorted = check_sorted(*stack);
+	if (!sorted)
+	{
+		while (count < 1)
+		{
+			if (stack->value < stack->next->value)
+			{
+				do_swap(stack);
+				ft_printf("sb\n");
+			}
+			else if (stack->value < get_lastval(*stack))
+			{
+				do_rev_rotate(stack);
+				ft_printf("rrb\n");
+			}
+			count++;
+		}
+	}
+}
+
+void	start_pushback(t_all **all)
+{
+	int			count;
+
+	count = 0;
+	while (all->sizeb || count)
+	{
+		if (all->stackb->value >  all->stacka->next->value)
+		{
+			do_rotate(&all->stacka);
+			ft_printf("ra\n");
+			count++;
+		}
+		else if (all->stackb->value < all->stacka->next->value && all->stackb->value > all->stacka->value)
+		{
+			do_push(&all->stacka, &all->stackb);
+			do_swap(&all->stacka);
+			ft_printf("pa\n");
+			ft_printf("sa\n");
+			all->sizeb -= 1;
+			all->sizea += 1;
+		}
+		else if (count && all->stackb->value < get_lastval(*all->stacka))
+		{
+			do_rev_rotate(&all->stacka);
+			count--;
+		}
+		else
+		{
+			do_push(&all->stacka, &all->stackb);
+			ft_printf("pa\n");
+			all->sizea += 1;
+			all->sizeb -= 1;
+		}
+	}
+}
+
 void	push_swap(t_all **all)
 {
 	int			sorted;
@@ -72,28 +135,27 @@ void	push_swap(t_all **all)
 		avaln = get_lastval(all->stacka);
 		if (all->sizea <= 3)
 			three_sort(&all->stacka, 'a');
-		if ((avalue > aval_one && aval_one > avaln) || avalue > avaln)
+		else if ((avalue > aval_one && aval_one > avaln) || avalue > avaln)
 		{
 			do_rev_rotate(all->stacka);
 			ft_printf("rra\n");
-			continue ;
 		}
 		else if (avalue > aval_one)
 		{
 			do_swap(all->stacka);
 			ft_printf("sa\n");
-			continue ;
 		}
 		else 
 		{
 			do_push(all->stackb, all->stacka);
 			all->sizea -= 1;
+			all->sizeb += 1;
 			ft_printf("pb\n");
-			sort_b(all->stackb); // make this function // sort elements as they are put into b
+			sort_b(all->stackb); // should try to find a way to make commands run alongside eachother epecially in the case of where we can use the double commands.
 		}
 		if (sorted = check_sorted(all->stacka))
-			start_pushback(all); // make this function
-		sorted = check_sorted(all->stacka)
+			start_pushback(all);
+		sorted = check_sorted(all->stacka);
 			// start pushback will take from b one at a time making sure the sort is still correct as you go
 	}
 }
